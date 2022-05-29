b862ad63694a1bb5291693fd423ea7337328a3f3
allexmorozovv <allexmorozovv@gmail.com>
2022-05-29 18:17:30 +0300

    add address in button

--- button/src/App.tsx
+++ button/src/App.tsx
@@ -14,11 +14,11 @@ function App() {
          console.log(num)
      }*/
 
-    const button1 = (subscriber: string, age: number) => {
-        console.log(subscriber + ' возраст:' + age)
+    const button1 = (subscriber: string, age: number, address: string) => {
+        console.log(subscriber + ' age:' + age)
     }
     const button2 = (subscriber: string, age: number) => {
-        console.log(subscriber + ' возраст:' + age)
+        console.log(subscriber + ' age:' + age)
     }
 
     const button3 = () => {
@@ -34,7 +34,7 @@ function App() {
         <button onClick={(event)=>for2(100200)}>2</button>*/}
             {/*<button>MyYouTubeChannel-1</button>
         <button>MyYouTubeChannel-2</button>*/}
-            <Button name={'MyYouTubeChannel-1'} callBack={() => button1('Im Vasya', 21)}/>
+            <Button name={'MyYouTubeChannel-1'} callBack={() => button1('Im Vasya', 21, ' Live in NY')}/>
             <Button name={'MyYouTubeChannel-2'} callBack={() => button2('Im Petya', 33)}/>
             <Button name={'Stupid BATTON'} callBack={button3}/>
         </div>
