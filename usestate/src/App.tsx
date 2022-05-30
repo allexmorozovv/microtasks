04a20382a7d2c1139a79adabbacf2843809bc86f
allexmorozovv <allexmorozovv@gmail.com>
2022-05-30 08:19:13 +0300

    add useState

---
+++ usestate/src/App.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+import './App.css';
+
+function App() {
+    // let a = 1
+    let [a, setA] = useState(1)
+    const onClickHandler = () => {
+        setA(++a)
+        console.log(a)
+    }
+    const onClickNull = () => {
+        setA(0)
+        console.log(a)
+    }
+    return (
+        <div className="App">
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>Number</button>
+            <button onClick={onClickNull}>0</button>
+        </div>
+    );
+}
+
+export default App;
